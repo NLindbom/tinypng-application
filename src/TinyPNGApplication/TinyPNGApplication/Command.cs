a518ed7ecfdbacc9b841a5fe027641d7ef13d32d
Niklas Lindbom <niklas.lindbom@hpinordic.se>
2017-10-13 20:16:40 +0200

    Application
    Signed-off-by: Niklas Lindbom <niklas.lindbom@hpinordic.se>

---
+++ src/TinyPNGApplication/TinyPNGApplication/Command.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TinyPNGApplication
+{
+    public abstract class BaseCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Func<bool> canExecute;
+
+        public BaseCommand(Func<bool> canExecute)
+        {
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (canExecute == null)
+                return true;
+
+            return canExecute.Invoke();
+        }
+
+        public abstract void Execute(object parameter);
+    }
+
+    public class Command : BaseCommand
+    {
+        private Action action;
+
+        public Command(Action execute, Func<bool> canExecute = null) : base(canExecute)
+        {
+            action = execute;
+        }
+
+        public override void Execute(object parameter)
+        {
+            action.Invoke();
+        }
+    }
+
+    public class Command<T> : BaseCommand where T : class
+    {
+        private Action<T> action;
+
+        public Command(Action<T> execute, Func<bool> canExecute = null) : base(canExecute)
+        {
+            action = execute;
+        }
+
+        public override void Execute(object parameter)
+        {
+            action.Invoke(parameter as T);
+        }
+    }
+}
---
+++ src/TinyPNGApplication/TinyPNGApplication/ZoomBorder.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace TinyPNGApplication
+{
+    /// <summary>
+    /// Border with child pan/zoom support credit to Wiesław Šoltés: https://stackoverflow.com/questions/741956/pan-zoom-image
+    /// </summary>
+    public class ZoomBorder : Border
+    {
+        /*public static readonly DependencyProperty RenderTransformProperty =
+            DependencyProperty.Register("Date", typeof(DateTime), typeof(DaiesContainer),
+            new PropertyMetadata(DateTime.Now, DatePropertyChanged));
+            */
+        private UIElement child = null;
+        private Point origin;
+        private Point start;
+
+        public override UIElement Child
+        {
+            get { return base.Child; }
+            set
+            {
+                if (value != null && value != Child)
+                    Initialize(value);
+
+                base.Child = value;
+            }
+        }
+
+        public void Initialize(UIElement element)
+        {
+            child = element;
+
+            if (child != null)
+            {
+                var transformGroup = new TransformGroup();
+
+                var scaleTransform = new ScaleTransform();
+                var translateTransform = new TranslateTransform();
+
+                transformGroup.Children.Add(scaleTransform);
+                transformGroup.Children.Add(translateTransform);
+
+                child.RenderTransform = transformGroup;
+
+                child.RenderTransformOrigin = new Point(0.0, 0.0);
+
+                this.MouseWheel += child_MouseWheel;
+                this.MouseLeftButtonDown += child_MouseLeftButtonDown;
+                this.MouseLeftButtonUp += child_MouseLeftButtonUp;
+                this.MouseMove += child_MouseMove;
+                this.PreviewMouseRightButtonDown += new MouseButtonEventHandler(child_PreviewMouseRightButtonDown);
+            }
+        }
+
+        public void Reset()
+        {
+            if (child != null)
+            {
+                // reset zoom
+                var scaleTransform = GetScaleTransform(child);
+                scaleTransform.ScaleX = 1.0;
+                scaleTransform.ScaleY = 1.0;
+
+                // reset pan
+                var translateTransform = GetTranslateTransform(child);
+                translateTransform.X = 0.0;
+                translateTransform.Y = 0.0;
+            }
+        }
+
+        private TranslateTransform GetTranslateTransform(UIElement element)
+        {
+            return (element.RenderTransform as TransformGroup).Children.First(tr => tr is TranslateTransform) as TranslateTransform;
+        }
+
+        private ScaleTransform GetScaleTransform(UIElement element)
+        {
+            return (element.RenderTransform as TransformGroup).Children.First(tr => tr is ScaleTransform) as ScaleTransform;
+        }
+
+        #region Child Events
+
+        private void child_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (child != null)
+            {
+                var scaleTransform = GetScaleTransform(child);
+                var translateTransform = GetTranslateTransform(child);
+
+                double zoom = e.Delta > 0 ? .2 : -.2;
+                if (!(e.Delta > 0) && (scaleTransform.ScaleX < .4 || scaleTransform.ScaleY < .4))
+                    return;
+
+                var relative = e.GetPosition(child);
+
+                double abosuluteX = relative.X * scaleTransform.ScaleX + translateTransform.X;
+                double abosuluteY = relative.Y * scaleTransform.ScaleY + translateTransform.Y;
+
+                scaleTransform.ScaleX += zoom;
+                scaleTransform.ScaleY += zoom;
+
+                translateTransform.X = abosuluteX - relative.X * scaleTransform.ScaleX;
+                translateTransform.Y = abosuluteY - relative.Y * scaleTransform.ScaleY;
+            }
+        }
+
+        private void child_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (child != null)
+            {
+                var translateTransform = GetTranslateTransform(child);
+
+                start = e.GetPosition(this);
+                origin = new Point(translateTransform.X, translateTransform.Y);
+
+                this.Cursor = Cursors.Hand;
+
+                child.CaptureMouse();
+            }
+        }
+
+        private void child_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (child != null)
+            {
+                child.ReleaseMouseCapture();
+
+                this.Cursor = Cursors.Arrow;
+            }
+        }
+
+        void child_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Reset();
+        }
+
+        private void child_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (child != null)
+            {
+                if (child.IsMouseCaptured)
+                {
+                    var translateTransform = GetTranslateTransform(child);
+
+                    Vector v = start - e.GetPosition(this);
+
+                    translateTransform.X = origin.X - v.X;
+                    translateTransform.Y = origin.Y - v.Y;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
